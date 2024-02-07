7acd08784eb6670589e2bc58e5a357fc2e3c4a2f
shashikant88093 <shashi94goswami@gmail.com>
2024-02-07 18:50:11 +0530

    Add .gitignore and package.json files, update App.js

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-const heading = React.createElement('div', { id: 'parent' }, 
-[
-    React.createElement('div', { id: 'child' }, 
-[
-    React.createElement('h1', {}, 'Hello, World!'),
-    React.createElement('h2', {}, 'React is awesome')
-]),
-React.createElement('div', { id: 'child2' }, 
-[
-    React.createElement('h1', {}, 'Hello, World!'),
-    React.createElement('h2', {}, 'React is awesome')
-])
-]
-
-);
-const root = document.getElementById('root');
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello, World!"),
+    React.createElement("h2", {}, "React is awesome"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello, World!"),
+    React.createElement("h2", {}, "React is awesome"),
+  ]),
+]);
+const root = document.getElementById("root");
 ReactDOM.render(heading, root);
 
 // const root = ReactDom.createRoot(document.getElementById('root'));
